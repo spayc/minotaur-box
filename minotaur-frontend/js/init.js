be710b1ba98637a7b767ad07f869e84b5aef19df
spayc <74024033+spayc@users.noreply.github.com>
2021-05-10 22:05:10 +0200

    from last repo

---
+++ minotaur-frontend/js/init.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+    $("#forgot-password").click(function(){
+        alert("Ye .... Thought it would be this easy? \n                       -_______-")
+    });
+    
+});
+
+
+
