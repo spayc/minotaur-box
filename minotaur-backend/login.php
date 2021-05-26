6649b2a22c5d5ae2a29ef5baba37e9298ace1f43
XenoxJeager <60194221+XenoxJeager@users.noreply.github.com>
2021-05-26 21:05:37 +0200

    Create login.php
    forgot to commit this

---
+++ minotaur-backend/login.php
@@ -0,0 +1,33 @@
+<?php
+    require("dbConnect.php");
+    session_start();
+    $error = false;
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        // username and password sent from form 
+        
+        $myusername = mysqli_real_escape_string($conn,$_POST['mail']);
+        $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
+        
+        $sql = "SELECT idLogin FROM labyrinth.login WHERE username = '$myusername' and passcode = '$mypassword'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $active = $row['active'];
+        
+        $count = mysqli_num_rows($result);
+        
+        // If result matched $myusername and $mypassword, table row must be 1 row
+          
+        if($count == 1) {
+           session_register("myusername");
+           $_SESSION['login_user'] = $myusername;
+           
+           header("Location: index.php");
+        }else {
+           $error = "Your Login Name or Password is invalid";
+        }
+     }
+    
+
+?>
