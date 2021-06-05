87e8f7379ac66b476eb95a7cf4508a6ad1f8ed5d
spayc <74024033+spayc@users.noreply.github.com>
2021-06-05 14:08:22 +0200

    commit

--- minotaur-backend/dbConnect.php
+++ minotaur-backend/dbConnect.php
@@ -7,12 +7,11 @@
 	
 	try {
 		$conn = new PDO("mysql:host=$servername;dbname=$db", $usr, $pwd);
-		// set the PDO error mode to exception
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		// echo "<p>Connected successfully</p>";
+
 		
 	} catch (PDOException $e) {
-		// echo "<p>Connection failed: " . $e->getMessage() . "</p>";
+		
 		die();
 	}
 
--- minotaur-frontend/js/init.js
+++ minotaur-frontend/js/init.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $("#forgot-password").click(function(){
         alert("Ye .... Thought it would be this easy? \n                       -_______-")
