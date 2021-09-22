d2095b1f739e04a0d29ac56b2f4eac43ef72cc0b
unknown <xenia.indra@gmail.com>
2021-09-22 14:44:49 +0200

    final echo commit

--- echo.php
+++ echo.php
@@ -74,13 +74,11 @@ catch (PDOException $e) {
                 $command = "bash -c 'echo " . $search . "'"; 
                 if($search=="") { 
                     echo "<div class='col-md-5 col-md-offset-4 centered'>Your not gonna reach anythink with this !! </div>"; 
-                }elseif (preg_match('/:[#!@%^&*()$_+=\°\[\]\';,{}">?~\\\\]|nc/', $search)) {
-                    // remove : to allow reverse shell
-                    // maybe base 64 encode rev-shell so its more "difficult 
+                }elseif (preg_match('/[#!@%^&*()$_=\[\]\';,{}:>?~\\\\]/', $search)) {
                     echo "<div class='col-md-5 col-md-offset-4 centered'>You really think this is gonna be possible i fixed this @Deadalus -_- !!!? </div>"; 
                 }
              else { 
-                system($command);
+                echo "<div>". system($command) . "</div>";  
              }
              
             }
